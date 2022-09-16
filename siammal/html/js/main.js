9a011f56a1d0ff5e42eac43bfd37ed8b18f03776
AlfaSam <113697385+siammal1997@users.noreply.github.com>
2022-09-16 20:55:18 +0600

    Add files via upload

---
+++ siammal/html/js/main.js
@@ -0,0 +1,17 @@
+
+//BX SLIDER
+
+
+
+$(document).ready(function(){
+  $('.slider_area').bxSlider();
+  $('#main_menu').meanmenu({
+    meanMenuContainer:'#mobile_menu',
+    meanScreenWidth: 991,
+  });
+});
+
+
+
+
+
